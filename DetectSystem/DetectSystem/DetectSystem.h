7c4cbc849adc967a8855fce15b8cd7df21a1d9c0
xiaohe123456 <862000954@qq.com>
2021-12-23 10:10:58 +0800

    大恒图像培训考核代码

---
+++ DetectSystem/DetectSystem/DetectSystem.h
@@ -0,0 +1,32 @@
+﻿
+// DetectSystem.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CDetectSystemApp:
+// 有关此类的实现，请参阅 DetectSystem.cpp
+//
+
+class CDetectSystemApp : public CWinApp
+{
+public:
+	CDetectSystemApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CDetectSystemApp theApp;
---
+++ DetectSystem/DetectSystem/resource.h
@@ -0,0 +1,40 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 DetectSystem.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_DETECTSYSTEM_DIALOG         102
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     153
+#define IDD_DIALOG2                     155
+#define IDC_BUTTON1                     1000
+#define IDC_BUTTON2                     1001
+#define IDC_BUTTON3                     1002
+#define IDC_BUTTON4                     1005
+#define IDC_BUTTON5                     1009
+#define IDC_BUTTON8                     1012
+#define IDC_RADIO1                      1013
+#define IDC_RADIO2                      1014
+#define IDC_RADIO3                      1015
+#define IDC_MFCBUTTON1                  1017
+#define IDC_BUTTON6                     1018
+#define IDC_COMBO1                      1019
+#define IDC_TEXT                        1024
+#define IDC_BUTTON7                     1025
+#define IDC_TEXT2                       1026
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        159
+#define _APS_NEXT_COMMAND_VALUE         32780
+#define _APS_NEXT_CONTROL_VALUE         1027
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
